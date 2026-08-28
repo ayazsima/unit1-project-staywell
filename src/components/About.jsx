59379b6ad5fdb376a93ec3ad063eccbd2fc2c86d
simaayazz <ayazsima3@gmail.com>
2026-08-27 21:33:15 -0500

    Add routing for pages

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+        <section>
+            <h2>About StayWell</h2>
+
+            <p>
+                StayWell helps users create a simple wellness plan based on
+                their wellness goal and the amount of time they have available.
+            </p>
+
+            <p>
+                The app provides healthy habits, daily activities, and supplement
+                information based on the user's selections.
+            </p>
+        </section>
+    );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
-        <header>
+        <header className="app-container">
             <h1>StayWell</h1>
+            <div>
+                <Link to="/">Home   </Link>
+                <Link to="/about">About    </Link>
+                <Link to="/saved-plan">Saved Plan</Link>
+            </div>
         </header>
     );
 }
